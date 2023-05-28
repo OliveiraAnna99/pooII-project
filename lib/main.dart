a5bc927ad0685a6eade08b81d0a5ee49275d8225
OliveiraAnna99 <anna.oliveira.700@ufrn.edu.br>
2023-05-28 08:38:45 -0300

    feat: usando bottomNavigationBar (#5)

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  
   MaterialApp app = MaterialApp(
-
-      theme: ThemeData(primarySwatch: Colors.red),
-
-      home: Scaffold(
-
-        appBar: AppBar(title: Text("Meu app")),
-
-        body: Center(
-
-          child: Column(
-
-            children: [
-
-              Text(
-                "Apenas começando...",
-                 style: TextStyle(height: 5, fontSize: 10),
-              
-              ),
-
-              Text("No meio..."),
-
-              Text("Terminando...")              
-
-            ]
-
-          )          
-
+    theme: ThemeData(primarySwatch: Colors.red),
+    home: Scaffold(
+      appBar: AppBar(title: Text("Meu app")),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              "Apenas começando...",
+              style: TextStyle(fontSize: 10, height: 1.5),
+              // Defina a altura desejada para o espaçamento entre as linhas
+         
+            ),
+            Text("No meio..."),
+            Text("Terminando..."),
+          ],
         ),
-
-        bottomNavigationBar: Text("Botão 1"),
-
-      ));
+      ),
+      bottomNavigationBar: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text("Texto 1"),
+          Text("Texto 2"),
+          Text("Texto 3"),
+        ],
+      ),
+    ),
+  );
 
   runApp(app);
-
 }
