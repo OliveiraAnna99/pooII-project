c881d13bb0d5511223a031cb225018e73297c0d9
Oliveira Anna <anna.oliveira.700@ufrn.edu.br>
2023-06-25 17:37:03 -0300

    feat: adicionando ScopeBinder

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'search_page.dart';
 import 'home_page.dart';
 import 'favorites_page.dart';
 import 'comic.dart'; // Importe a classe Comic
+import 'package:binder/binder.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,13 +15,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: InitialPage(),
-      routes: {
-        '/home': (context) => HomePage(),
-        '/search': (context) => SearchPage(),
-        '/favorites': (context) => FavoritePage(favoriteComics: favoriteComics),
-      },
+    return BinderScope(
+      child: MaterialApp(
+        home: InitialPage(),
+        routes: {
+          '/home': (context) => HomePage(),
+          '/search': (context) => SearchPage(),
+          '/favorites': (context) => FavoritePage(favoriteComics: favoriteComics),
+        },
+      ),
     );
   }
 }
