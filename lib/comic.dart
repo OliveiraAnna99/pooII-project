7c46b9a6f5f781a7e49e747c7d5f0d949a495e3c
Oliveira Anna <anna.oliveira.700@ufrn.edu.br>
2023-06-25 10:15:53 -0300

    feat: modularizando parte 1

---
+++ lib/comic.dart
@@ -0,0 +1,15 @@
+class Comic {
+  final int id;
+  final String title;
+  final String description;
+  final String image;
+  bool isFavorite;
+
+  Comic({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.image,
+    this.isFavorite = false,
+  });
+}
